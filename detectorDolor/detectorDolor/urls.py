62a9c12057a8b81ed76fc815be9cdbc701784298
KevinMerinoZ <novuskevinz@gmail.com>
2025-11-04 17:10:57 -0600

    Implementando inicio de sesion

--- detectorDolor/detectorDolor/urls.py
+++ detectorDolor/detectorDolor/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from inicioSesion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('usuario.urls')),
+    path('', views.login),
 ]
---
+++ detectorDolor/inicioSesion/urls.py
@@ -0,0 +1,3 @@
+from django.urls import auth
+from . import views
+
